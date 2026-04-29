97dba05d5c813623769a46474bc29c9c16d4b8aa
BRIAN MWANGI <mambichwabrayo@gmail.com>
2026-04-29 11:38:05 +0300

    Add callback handler for POST requests

---
+++ api/callback.js
@@ -0,0 +1,17 @@
+// api/callback.js
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method Not Allowed' });
+  }
+
+  const data = req.body;
+  
+  console.log('=== PAYHERO CALLBACK ===');
+  console.log(data);
+  console.log('========================');
+
+  res.status(200).json({ 
+    ResultCode: 0, 
+    ResultDesc: 'Received' 
+  });
+}
